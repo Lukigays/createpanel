bc86e58ec214f99d686a8928c673228ab2ee0c2c
Lukigays <luki85030@gmail.com>
2024-12-14 15:28:53 +0700

    https://github.com/Lukigays/createpanel

---
+++ settings.js
@@ -0,0 +1,21 @@
+const settings = {
+  token: 'tokenbotmu', // Token Bot
+  adminId: '7672169037',
+  urladmin: 'https://t.me/rexxadev',
+  pp: 'https://e.top4top.io/p_3231p45yk0.jpg',
+    //SERVER 1
+  domain: 'rexxawangi',
+  plta: 'rexxawangi',
+  pltc: 'rexxawangi',
+  
+   //SERVER 2
+  domain2: 'rexxawangi',
+  plta2: 'rexxawangi',
+  pltc2: 'rexxawangi',
+  
+  //CREATE PANEL
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
